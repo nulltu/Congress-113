e35706d5fa3f8662082cbd233b6e0553f9bd1160
nulltu <rusbent.matta@gmail.com>
2020-07-29 22:50:36 -0300

    casi la mitad ok

--- js/statistics.js
+++ js/statistics.js
@@ -1,20 +1,20 @@
 $(document).ready(
-//llamada a la API
-$.ajax({
-  url:'https://api.propublica.org/congress/v1/113/house/members.json',
-  headers:{
-    "X-API-Key":"AR98vgR94I7lK8uAuqTlHA601UD3yPNiN0uMG0TY"
-  },
+  //llamada a la API
+  $.ajax({
+    url: 'https://api.propublica.org/congress/v1/113/house/members.json',
+    headers: {
+      "X-API-Key": "AR98vgR94I7lK8uAuqTlHA601UD3yPNiN0uMG0TY"
+    },
 
-  success: function (dataHouse){
-    dataHouse=dataHouse.results[0].members 
-    myProgram(dataHouse)
+    success: function (myJsonHouse) {
+      myJsonHouse = myJsonHouse.results[0].members
+      myProgram(myJsonHouse)
 
-  }
-}))
+    }
+  }))
 
-/*function myProgram(dataHouse){
-for (let valor of dataHouse) {
+/*function myProgram(myJsonHouse){
+for (let valor of myJsonHouse) {
   membersHouse.innerHTML += `
         <tr>
         <td scope="row" class="text-primary font-weight-normal"><a href="${valor.url}">${valor.last_name + " " +
@@ -29,60 +29,135 @@ for (let valor of dataHouse) {
 }*/
 
 
-function myProgram(dataHouse){
+function myProgram(myJsonHouse) {
   var app = new Vue({
-    el:'#app',
-    data:{
-      demoHouse:0,
-      repHouse:0,
-      indHouse:0,
-      totalHouse:0,
-      pvr:0
+    el: '#app',
+    data: {
+      demoHouse: 0,
+      repHouse: 0,
+      indHouse: 0,
+      totalHouse: 0,
+      pvrHouse: 0,
+      pvdHouse:0,
+      pviHouse:0,
+      leastEnHouse:[],
+      leastLoyalHouse:[],
+      numberPartyVotesHouse:0
     }
   })
-
-
 
   if (document.querySelector('#app')) {
 
-  dataHouse.map(party => {
-    if (party.party == 'R') {
-      app.demoHouse++
-      console.log(app.demoHouse)
+    myJsonHouse.map(party => {
+      if (party.party == 'R') {
+        app.demoHouse++
+        //console.log(app.demoHouse)
+      }
+      else if (party.party == 'D') {
+        app.repHouse++
+      }
+      else if (party.party == 'ID') {
+        app.indHouse++
+      }
+    })
+
+    //Sumatoria de miembros
+    app.totalHouse = app.demoHouse + app.indHouse + app.repHouse
+
+    //console.log(app.demoHouse)
+    var listRepHouse = myJsonHouse.filter(member => member.party == 'R')
+
+    //Función que recorre el array filtrado anteriormente, sumo y promedio.
+    var sumRepHouse = 0;
+    for (let value in listRepHouse) {
+      sumRepHouse += listRepHouse[value].votes_with_party_pct
     }
-    else if (party.party == 'D') {
-      app.repHouse++
+    //console.log(sumRepHouse)
+    //Le pongo !==0 para que en la tabla no me de NaN si es el valor de miembros en el partido es ="0".
+    if (sumRepHouse !== 0) {
+      //Guardo en el objeto estadistica la propiedad Porcentaje de Republicanos redondenado a a cifras.
+      app.pvrHouse = (sumRepHouse / listRepHouse.length).toFixed(2)
+    } else {
+      app.pvrHouse = 0
     }
-    else if (party.party == 'ID') {
-      app.indHouse++
+    //console.log(app.pvr);
+
+    var listDemHouse = myJsonHouse.filter(member => member.party == 'D')
+    var sumDemHouse = 0;
+    for (let value in listDemHouse) {
+      sumDemHouse += listDemHouse[value].votes_with_party_pct
     }
-  })
+    if (sumDemHouse !== 0) {
+      app.pvdHouse = (sumDemHouse / listDemHouse.length).toFixed(2)
+    } else {
+      app.pvdHouse = 0
+    }
+
+    var listIndHouse = myJsonHouse.filter(member => member.party == 'ID')
+    var sumIndHouse = 0;
+    for (let value in listIndHouse) {
+      sumIndHouse += listIndHouse[value].votes_with_party_pct
+    }
+    if (sumIndHouse !== 0) {
+      app.pviHouse = (sumIndHouse / listIndHouse.length).toFixed(2)
+    } else {
+      app.pviHouse = 0
+    }
 
 
-app.totalHouse = app.demoHouse+app.indHouse+app.repHouse
+  var percTenHouse = Math.round((myJsonHouse.length * 10) / 100)
+  //console.log(percTenHouse)
 
-console.log(app.demoHouse)
+// House - Least Engaged 
+var orderHouseLeast = myJsonHouse.sort((a, b) => (b.missed_votes_pct - a.missed_votes_pct))
 
-var listRep = myJsonHouse.filter(member => member.party == 'R')
-
-//Función que recorre el array filtrado anteriormente, sumo y promedio.
-var sumRepHouse = 0;
-for (let value in listRep) {
-  sumRepHouse += listRep[value].votes_with_party_pct
+for (var i = 0; i < percTenHouse; i++) {
+  app.leastEnHouse.push(orderHouseLeast[i])
 }
-//console.log(sumRepHouse)
 
-//Le pongo !==0 para que en la tabla no me de NaN si es el valor de miembros en el partido es ="0".
-if (sumRepHouse !== 0) {
-  //Guardo en el objeto estadistica la propiedad Porcentaje de Republicanos redondenado a a cifras.
-  app.pvr = (sumRepHouse / listRep.length).toFixed(2)
-} else {
-  app.pvr = 0
-}
 
-console.log(app.pvr);
+  //Ordeno los miembros de acuerdo a la cantidad de votos para con su partido
+  var orderPartyHouse = myJsonHouse.sort((a, b) => (a.votes_with_party_pct - b.votes_with_party_pct))
+  //console.log(orderPartyHouse)
 
-}
+  for (var i = 0; i < percTenHouse; i++) {
+    app.leastLoyalHouse.push(orderPartyHouse[i])
+  }
+ console.log(app.leastLoyalHouse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
 }
 
 
